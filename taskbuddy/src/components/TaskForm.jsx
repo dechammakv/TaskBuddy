14bd21d909b9706697304cbbc5f29b164e5b811c
dechammakv <dechammakarthamada8@gmail.com>
2024-12-23 15:47:52 +0530

    Add new files

---
+++ taskbuddy/src/components/TaskForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function TaskForm() {
+  const [task, setTask] = useState("");
+  return (
+    <form action="">
+      <input
+        type="text"
+        placeholder="Enter Your Task"
+        required
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <h1>{task}</h1>
+    </form>
+  );
+}
